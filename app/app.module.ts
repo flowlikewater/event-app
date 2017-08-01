6b4befc365e5198351341ecb6b35eb4d390985c4
Kevin <cho.kevin@hotmail.com>
2017-08-02 01:01:24 +0800

    create event form complete

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import {
     EventsListComponent,
@@ -11,16 +12,18 @@ import {
     EventRouteActivator,
     EventListResolver
 } from './events/index'
- 
 import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { ToastrService } from './common/toastr.service'
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
+import { AuthService } from './user/auth.service'
 
 @NgModule ({
     imports: [
         BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot(appRoutes)
     ],
     declarations: [
@@ -36,6 +39,7 @@ import { Error404Component } from './errors/404.component'
         EventService,
         ToastrService,
         EventRouteActivator,
+        AuthService,
         EventListResolver,
         { 
             provide: 'canDeactivateCreateEvent', 
