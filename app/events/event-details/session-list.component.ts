2abd6325f9393b2940ab59c949a91662dafdda9a
Kevin <cho.kevin@hotmail.com>
2017-08-02 23:00:28 +0800

    sorting by vote and name for event sessions done

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -9,11 +9,13 @@ import { ISession } from '../shared/index'
 export class SessionListComponent implements OnChanges {
     @Input() sessions:ISession[];
     @Input() filterBy:string;
+    @Input() sortBy:string;
     visibleSessions:ISession[]=[]
 
     ngOnChanges(){
         if(this.sessions){
             this.filterSessions(this.filterBy)
+            this.sortBy === 'name' ? this.visibleSessions.sort(soryByNameAsc) : this.visibleSessions.sort(sortByVotesDesc)
         }
     }
     filterSessions(filter){
@@ -24,4 +26,14 @@ export class SessionListComponent implements OnChanges {
             })
         }
     }
+}
+
+function soryByNameAsc(s1:ISession, s2:ISession){
+    if(s1.name>s2.name) return 1
+    else if(s1.name === s2.name) return 0
+    else return -1
+}
+
+function sortByVotesDesc(s1:ISession, s2:ISession){
+    return s2.voters.length - s1.voters.length
 }
